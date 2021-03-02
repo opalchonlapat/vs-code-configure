d8be58d7974f36d87743eb786f91ec4e562bffdd
Chonlapat Patanajirasit <chonlapat@central.tech>
2021-03-02 12:28:42 +0700

    first configure

---
+++ configure.sh
@@ -0,0 +1,15 @@
+code --install-extension CoenraadS.bracket-pair-colorizer
+code --install-extension dracula-theme.theme-dracula
+code --install-extension formulahendry.code-runner
+code --install-extension humao.rest-client
+code --install-extension kuscamara.electron
+code --install-extension mechatroner.rainbow-csv
+code --install-extension ms-azuretools.vscode-docker
+code --install-extension ms-python.anaconda-extension-pack
+code --install-extension ms-python.python
+code --install-extension ms-python.vscode-pylance
+code --install-extension ms-toolsai.jupyter
+code --install-extension neuron.neuron-IPE
+code --install-extension redhat.vscode-yaml
+code --install-extension ritwickdey.LiveServer
+code --install-extension techer.open-in-browser
